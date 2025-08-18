4994cf7d1c9958f9afb36b639e60eaa1f48ccdd5
Gilbert46 <67496732+Gilbert46@users.noreply.github.com>
2025-08-18 22:03:47 +0200

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+document.querySelector('#sele').onready = Dllfile
+
+var id = document.getElementById("load")
+//id.innerHTML = '</br><a href="./src/dll/sqlite3.dll">repository</a>'
+id.innerHTML = '<a href="src/dll/sqlite3.dll" rel="dll"></a>'
+/*
+var a = document.createElement('a')
+a.innerHTML = "dowload_events"
+a.addEventListener(
+    "click",
+    function (ev) {
+      a.href = "./src/dll/sqlite3.dll"
+    },
+    false,
+  );
+document.body.appendChild(a)
+*/
+function Dllfile() {
+  console.log("init download")
+  
+}
+
+
+console.log("hello world")
