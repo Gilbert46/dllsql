c9e5da7e10c8a50e12bd595db298b0f61fe0caa3
Gilbert46 <67496732+Gilbert46@users.noreply.github.com>
2025-08-19 14:13:18 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 document.querySelector('#sele').onready = Dllfile
 
 var id = document.getElementById("load")
-//id.innerHTML = '</br><a href="./src/dll/sqlite3.dll">repository</a>'
-id.innerHTML = '<a href="src/dll/sqlite3.dll" rel="dll"></a>'
+//id.innerHTML = '</br><a href="./src/dll/sqlite3.zip">repository</a>'
+id.innerHTML = '<a href="src/dll/sqlite3.zip" rel="dll"></a>'
 /*
 var a = document.createElement('a')
 a.innerHTML = "dowload_events"
@@ -19,6 +19,7 @@ function Dllfile() {
   console.log("init download")
   
 }
+
 
 
 console.log("hello world")
